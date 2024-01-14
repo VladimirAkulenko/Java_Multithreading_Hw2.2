46f4cb1d7d3ca35c06b3abac5112ce61612f9539
Vladimir Akulenko <vladimirakulenk@yandex.ru>
2024-01-14 15:25:20 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+    public static final int BUYERS = 10;
+
+    public static void main(String[] args) {
+        final Dealer dealer = new Dealer();
+        for (int i = 1; i < BUYERS; i++) {
+            new Thread(null, dealer::sellCar, "Покупатель" + i).start();
+        }
+        new Thread(null, dealer::receiveCar, "Toyota").start();
+    }
+}
